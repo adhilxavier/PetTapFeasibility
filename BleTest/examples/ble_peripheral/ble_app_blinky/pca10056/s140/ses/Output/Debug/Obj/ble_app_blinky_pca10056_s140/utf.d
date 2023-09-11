7129efbe2c4dd942d737bdbdfc7b81e81a15b0f4
adhilxavier <adhilxavier@gmail.com>
2023-09-11 14:52:26 +0530

    Initial commmit

---
+++ BleTest/examples/ble_peripheral/ble_app_blinky/pca10056/s140/ses/Output/Debug/Obj/ble_app_blinky_pca10056_s140/utf.d
@@ -0,0 +1,9 @@
+Output/Debug/Obj/ble_app_blinky_pca10056_s140/utf.o: \
+ /home/alcodex/Desktop/nrf/Test/nrf5_sdk_17.1.0_ddde560/nRF5_SDK_17.1.0_ddde560/external/utf_converter/utf.c \
+ /home/alcodex/Desktop/nrf/Test/nrf5_sdk_17.1.0_ddde560/nRF5_SDK_17.1.0_ddde560/external/utf_converter/utf.h \
+ /usr/share/segger_embedded_studio_for_arm_7.10/include/stddef.h \
+ /usr/share/segger_embedded_studio_for_arm_7.10/include/__SEGGER_RTL.h \
+ /usr/share/segger_embedded_studio_for_arm_7.10/include/__SEGGER_RTL_ConfDefaults.h \
+ /usr/share/segger_embedded_studio_for_arm_7.10/include/__SEGGER_RTL_Conf.h \
+ /usr/share/segger_embedded_studio_for_arm_7.10/include/__SEGGER_RTL_Arm_Conf.h \
+ /usr/share/segger_embedded_studio_for_arm_7.10/include/stdint.h
